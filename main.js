8abfc7f0bd3b931068b7e6a004eb3fe9a1cd5356
mehedi892 <mehedi@efoli.com>
2022-03-31 12:02:56 +0600

    homeComplete

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ let mainMenuDiv = document.getElementById('navMenu');
 mobileMenuIcon.addEventListener('click', () =>{
     mainMenuDiv.classList.toggle("mobile");
 });
+
+///Slider Our Valuable Clients
+let divGrid= document.getElementById('valueClientDiv1GridId');
+
+let divAll = divGrid.getElementsByClassName('valueClientDiv1GridDivCmn');
+
+
+let rightArrow = document.getElementById('arrow-right');
+let leftArrow = document.getElementById('arrow-left');
+
+
+rightArrow.addEventListener('click', function(){
+    
+    divGrid.scrollLeft += 125;
+});
+
+leftArrow.addEventListener('click', function(){
+    divGrid.scrollLeft -= 125;
+    
+});
+
+let maxScrollLeft = divGrid.scrollWidth - divGrid.clientWidth;
+function autoPlay(){
+    if(divGrid.scrollLeft > (maxScrollLeft - 1)){
+        divGrid.scrollLeft -= maxScrollLeft;
+
+    }
+    else{
+        divGrid.scrollLeft += 1;
+    }
+}
+
+///Slider Our Valuable Clients end
