34121c6658202a85cf65595e2dab678c4e029b36
mehedi892 <mehedi@efoli.com>
2022-04-04 03:45:09 +0600

    revire slider

---
+++ data.js
@@ -0,0 +1,99 @@
+const data = {
+    "data":[
+        {
+            "id" : "0",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "Mehedi",
+        "company" :"",
+        "starImg" : "",
+        },
+
+        {
+            "id" : "1",
+        "img" : "resources/protoImages/client 2.png",
+        "details" : "Trapa",
+        "company" :"",
+        "starImg" : "",
+        },
+
+
+        {
+            "id" : "2",
+        "img" : "resources/protoImages/client 3.png",
+        "details" : "",
+        "company" :"Fatema",
+        "starImg" : "",
+        },
+
+
+        {
+            "id" : "3",
+        "img" : "resources/protoImages/client 2.png",
+        "details" : "",
+        "company" :"Touhid",
+        "starImg" : "",
+        },
+
+
+        {
+            "id" : "4",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+        {
+            "id" : "5",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+        {
+            "id" : "6",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+
+        {
+            "id" : "7",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+        {
+            "id" : "8",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+        {
+            "id" : "9",
+        "img" : "resources/protoImages/client 1.png",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+        {
+            "id" : "10",
+        "img" : "resources/protoImages/client6.jpg",
+        "details" : "",
+        "company" :"JA",
+        "starImg" : "",
+        },
+
+        
+
+
+    ]
+}
--- main.js
+++ main.js
@@ -39,3 +39,77 @@ function autoPlay(){
 }
 
 ///Slider Our Valuable Clients end
+
+
+/// People say slider
+const reviewData = data.data;
+console.log(reviewData[0].img);
+let id = 0;
+let id1 = 1;
+let id2 = 2;
+let id3 = 3;
+let id4 =4;
+
+
+function loadReview (){
+ 
+    const reviewDiv = document.querySelector('.clientReviewDiv1GridCmn');
+    reviewDiv.innerHTML = `<div>
+    <img src="${reviewData[id].img}" alt="" class="clientReviewDiv1GridCmnImg" id="img1">
+</div>
+
+
+
+<div>
+    <img src="${reviewData[id1].img}" alt="" class="clientReviewDiv1GridCmnImg" id="img2">
+</div>
+
+
+
+<div>
+    <img src="${reviewData[id2].img}" alt="" class="clientReviewDiv1GridCmnImg" id="img3">
+   
+</div>
+
+
+
+<div>
+    <img src="${reviewData[id3].img}" alt="" class="clientReviewDiv1GridCmnImg" id="img4">
+</div>
+
+
+
+<div>
+    <img src="${reviewData[id4].img}" alt="" class="clientReviewDiv1GridCmnImg" id="img5">
+</div>
+`
+
+
+
+}
+
+loadReview();
+
+function slideRight (){
+    id = id + 1;
+    id1 = id1 + 1;
+    id2 = id2 + 1;
+    id3 = id3 + 1;
+    id4 = id4 + 1;
+   
+    if(reviewData.length-5<id4){
+        id4 = id4 -4;
+        if(id4>0){
+          id4= 0;
+        }
+    }
+
+    else if(reviewData.length-4<id3){
+        id4 = id4 -3;
+    }
+    console.log(id,id1,id2,id3,id4);
+   
+    loadReview();
+
+   
+}
